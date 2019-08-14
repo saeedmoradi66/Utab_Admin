b733897f179104e67896f069793d2bc020745f0a
سعيد مرادي <smoradi@sanjesh.org>
2019-08-14 16:01:12 +0430

    23

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { UsersComponent } from './users/users.component';
 import { AppRoutingModule } from './app-routing.module';
+import { SidebarMenuComponent } from './sidebar-menu/sidebar-menu.component';
+import { TopNavigationComponent } from './top-navigation/top-navigation.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, UsersComponent],
+  declarations: [AppComponent, HomeComponent, UsersComponent, SidebarMenuComponent, TopNavigationComponent, FooterComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    
   ],
   providers: [],
   bootstrap: [AppComponent]
