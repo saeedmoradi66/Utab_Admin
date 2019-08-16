c8721fa69b38b3d6cbd3e15d92b0d6154f6e616d
سعید مرادی <saeed.moradi3@gmail.com>
2019-08-17 00:34:34 +0430

    newtheme

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
+import { TopNavigationComponent } from './top-navigation/top-navigation.component';
+import { SideBarComponent } from './side-bar/side-bar.component';
 import { UsersComponent } from './users/users.component';
 import { AppRoutingModule } from './app-routing.module';
-import { SidebarMenuComponent } from './sidebar-menu/sidebar-menu.component';
-import { TopNavigationComponent } from './top-navigation/top-navigation.component';
-import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, UsersComponent, SidebarMenuComponent, TopNavigationComponent, FooterComponent],
-  imports: [
-    BrowserModule,
-    AppRoutingModule,
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    TopNavigationComponent,
+    SideBarComponent,
+    UsersComponent
   ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
 })
